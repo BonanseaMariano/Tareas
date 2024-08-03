1e7c19d2c778e075fb71dea4ee6fef64ba88ab70
Mariano Bonansea <marianobonanseapetrovial@gmail.com>
2024-08-03 10:53:48 -0300

    Services

---
+++ src/main/java/com/mycompany/controllers/IndexController.java
@@ -0,0 +1,119 @@
+package com.mycompany.controllers;
+
+import com.mycompany.models.Tarea;
+import com.mycompany.services.TareaServicio;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.PropertyValue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+@Component
+public class IndexController implements Initializable {
+    private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
+
+    @Autowired
+    private TareaServicio tareaServicio;
+
+    @javafx.fxml.FXML
+    private TableColumn<Tarea, String> nombreTareaColumna;
+
+    @javafx.fxml.FXML
+    private TableColumn<Tarea, String> estatusTareaColumna;
+
+    @javafx.fxml.FXML
+    private TableColumn<Tarea, String> responsableTareaColumna;
+
+    @javafx.fxml.FXML
+    private TableColumn<Tarea, Integer> idTareaColumna;
+
+    @javafx.fxml.FXML
+    private TableView<Tarea> tareaTabla;
+
+    private final ObservableList<Tarea> tareaList = FXCollections.observableArrayList();
+
+    @javafx.fxml.FXML
+    private TextField responsableTareaTexto;
+
+    @javafx.fxml.FXML
+    private Button eliminarBoton;
+
+    @javafx.fxml.FXML
+    private Button ltBoton;
+
+    @javafx.fxml.FXML
+    private Button modificarBoton;
+
+    @javafx.fxml.FXML
+    private TextField estatusTareaTexto;
+
+    @javafx.fxml.FXML
+    private Button agregarBoton;
+
+    @javafx.fxml.FXML
+    private TextField nombreTareaTexto;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        tareaTabla.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        configurarColumnas();
+        listarTareas();
+    }
+
+    private void configurarColumnas() {
+        idTareaColumna.setCellValueFactory(new PropertyValueFactory<>("idTarea"));
+        nombreTareaColumna.setCellValueFactory(new PropertyValueFactory<>("nombreTarea"));
+        responsableTareaColumna.setCellValueFactory(new PropertyValueFactory<>("responsable"));
+        estatusTareaColumna.setCellValueFactory(new PropertyValueFactory<>("estatus"));
+    }
+
+    private void listarTareas() {
+        logger.info("Listando tareas");
+        tareaList.clear();
+        tareaList.addAll(tareaServicio.listarTareas());
+        tareaTabla.setItems(tareaList);
+    }
+
+    @javafx.fxml.FXML
+    public void agregarTarea() {
+        if (nombreTareaTexto.getText().isEmpty() || responsableTareaTexto.getText().isEmpty() || estatusTareaTexto.getText().isEmpty()) {
+            mostrarMensaje("Error", "Todos los campos son obligatorios", Alert.AlertType.ERROR);
+            return;
+        }
+
+        Tarea tarea = new Tarea();
+        recolectarDatosFormulario(tarea);
+        tareaServicio.guardarTarea(tarea);
+        mostrarMensaje("Informacion", "Tarea guardada con exito", Alert.AlertType.INFORMATION);
+        limpiarFormulario();
+        listarTareas();
+    }
+
+    private void mostrarMensaje(String titulo, String mensaje, Alert.AlertType alertType) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(titulo);
+        alert.setHeaderText(null);
+        alert.setContentText(mensaje);
+        alert.showAndWait();
+    }
+
+    private void recolectarDatosFormulario(Tarea tarea) {
+        tarea.setNombreTarea(nombreTareaTexto.getText());
+        tarea.setResponsable(responsableTareaTexto.getText());
+        tarea.setEstatus(estatusTareaTexto.getText());
+    }
+
+    private void limpiarFormulario() {
+        nombreTareaTexto.clear();
+        responsableTareaTexto.clear();
+        estatusTareaTexto.clear();
+    }
+}
---
+++ src/main/java/com/mycompany/presentacion/SistemaTareasFx.java
@@ -0,0 +1,39 @@
+package com.mycompany.presentacion;
+
+import com.mycompany.TareasApplication;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+public class SistemaTareasFx extends Application {
+
+    private ConfigurableApplicationContext applicationContext;
+//    public static void main(String[] args) {
+//        launch(args);
+//    }
+    @Override
+    public void init(){
+        this.applicationContext = new SpringApplicationBuilder(TareasApplication.class).run();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(TareasApplication.class.getResource("/templates/Index.fxml"));
+        loader.setControllerFactory(applicationContext::getBean);
+        Scene escena = new Scene(loader.load());
+        stage.setScene(escena);
+        stage.show();
+    }
+
+    @Override
+    public void stop(){
+        applicationContext.close();
+        Platform.exit();
+    }
+}
